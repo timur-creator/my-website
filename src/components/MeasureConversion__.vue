961381c277bcc53e1b9c3064345fe4fffbcbfbee
timur-creator <68447267+timur-creator@users.noreply.github.com>
2020-08-18 11:52:03 +0200

    add Project

---
+++ src/components/MeasureConversion__.vue
@@ -0,0 +1,31 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "MeasureConversion",
+  data() {
+    return {
+/*      currentCategory: "Weight",
+      category: "Weight",
+      currentUnit: "kg",
+      endUnit: "pound",
+      currentUnits: "Metric",
+      endUnits: "Avoirdupois",*/
+
+    };
+  },
+  mounted() {
+
+
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style lang="scss">
+  @import "../../public/Measure-conversion/Measure-conversion.scss";
+</style>
